af685f34f93aec15fd708fe98d5cbd420a6bd616
Kirt <itsukirt@gmail.com>
2022-07-02 16:44:05 +0800

    fix sqlx

--- dao/mysql/mysql.go
+++ dao/mysql/mysql.go
@@ -6,19 +6,19 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-var db *sqlx.DB
+var DB *sqlx.DB
 
 func InitDB() (err error) {
 	dsn := "root:dangerous@tcp(127.0.0.1:3306)/bluebell?charset=utf8mb4&parseTime=True&loc=Local"
 	//sqlx.Connect: Open and Ping
-	db, err = sqlx.Connect("mysql", dsn)
+	DB, err = sqlx.Connect("mysql", dsn)
 	if err != nil {
 		fmt.Println("connect to mysql failed, err:", err)
 		return
 	}
 
-	db.SetMaxOpenConns(200)
-	db.SetConnMaxIdleTime(10)
+	DB.SetMaxOpenConns(200)
+	DB.SetConnMaxIdleTime(10)
 
 	return
 }
